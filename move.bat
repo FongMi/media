c7b64397f79e3bfb4bdea9e3ef614e1ab39b2b4b
FongMi <b8892778@gmail.com>
2026-03-15 14:06:46 +0800

    Optimize build tool

--- move.bat
+++ move.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 set "source_dir=D:\Project\media\libraries"
 set "target_dir=D:\Project\TV\app\libs"
 set "move_list=D:\Project\media\move.txt"
@@ -6,9 +7,7 @@ set "move_list=D:\Project\media\move.txt"
 for /r "%source_dir%" %%a in (lib-*-release.aar) do (
     findstr /x /c:"%%~nxa" "%move_list%" >nul
     if not errorlevel 1 (
-        copy "%%a" "%target_dir%\"
+        copy "%%a" "%target_dir%\" >nul
         echo Moved "%%a" to "%target_dir%"
-    ) else (
-        echo Skipped "%%a" as it is not listed in move.txt
     )
 )
